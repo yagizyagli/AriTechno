6fc89420c6635770b7e93d22bad3e0c863250d0c
sumeyyeyener <sumeyyener.0@gmail.com>
2026-05-12 21:31:23 +0300

    aritechno başlangıç

---
+++ AriTechno.Web/Controllers/AnasayfaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AriTechno.Web.Controllers
+{
+    public class AnasayfaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
