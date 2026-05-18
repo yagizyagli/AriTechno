1313df40b4fb8f3d5f089ee6e2b30f7c569ef7d7
sumeyyeyener <sumeyyener.0@gmail.com>
2026-05-18 19:39:02 +0300

    Database katman için entities eklemesi

---
+++ AriTechno.Database/Entities/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace AriTechno.Database.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        public string Adi { get; set; }
+
+        public string Aciklama { get; set; }
+    }
+}
---
+++ AriTechno.Database/Entities/Product.cs
@@ -0,0 +1,25 @@
+﻿namespace AriTechno.Database.Entities
+{
+    public class Product
+    {
+
+        public int Id { get; set; }
+
+        public string Adi { get; set; }
+
+        public decimal Fiyat { get; set; } 
+
+        public int Stok { get; set; }
+
+        public string Aciklama { get; set; }
+
+        public DateTime EklemeTarihi { get; set; }
+
+        public int EkleyenId { get; set; }
+
+        public DateTime GuncellemeTarihi { get; set; }
+
+        public int GuncelleyenId { get; set; }
+
+    }
+}
