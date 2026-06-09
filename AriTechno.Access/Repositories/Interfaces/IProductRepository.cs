0d2dc102b891c52ee52c6e842cbeeb30da6b2bd9
sumeyyeyener <sumeyyener.0@gmail.com>
2026-06-09 21:35:50 +0300

    List için tablo,Access repoları yapıldı

---
+++ AriTechno.Access/Repositories/Interfaces/IProductRepository.cs
@@ -0,0 +1,8 @@
+﻿using AriTechno.Database.Entities;
+
+namespace AriTechno.Access.Repositories.Interfaces
+{
+    internal interface IProductRepository::TEntity<Product>
+    {
+    }
+}
---
+++ AriTechno.Access/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace AriTechno.Access.Repositories.Interfaces;
+
+internal interface IRepository:<TEntity> where TEntity : class
+{
+    public void Save(string name, string description);
+    public void Update(int id, string name, string description);
+    public void Delete(int id);
+    public List<TEntity> GetAll();
+    public TEntity GetById(int id);
+    public List<TEntity> GetByName(string name);
+}
